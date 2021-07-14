495aa39f11f46213b646bc0c8cdc901265570a66
makabaka-wxidixi <537510672@qq.com>
2021-07-14 11:40:29 +0800

    输入一个数a，和n。将会得到a+aa+aaa+aaaa+n个a，的和

---
+++ 求和.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int sum(int a, int n)
+{
+	int i = 0;
+	int sum = 0;
+	int b = 0;
+	for (i = 0; i < n; i++)
+	{
+		b = b * 10 + 2;
+		sum += b;
+	}
+	return sum;
+}
+int main()
+{
+	int SN;
+	int a;
+	int n;
+	printf("例如：a=2,n=3,可以得到2+22+222的和\n");
+	printf("请输入a和n\n");
+	scanf("%d%d", &a,&n);
+	SN = sum(a, n);
+	printf("%d\n", SN);
+	return 0;
+}
