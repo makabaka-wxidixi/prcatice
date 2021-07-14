fd5bf4ad8752d62da556f3e70ff046d3ac8154cb
makabaka-wxidixi <537510672@qq.com>
2021-07-14 17:13:51 +0800

    喝汽水问题

---
+++ 喝汽水问题.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int drink_max(int bottle, int exc)
+{
+	int count = bottle;
+	while (bottle > 1)
+	{
+		if (bottle % 2 == 0)
+		{
+			bottle = bottle / exc;
+			count += bottle;
+		}
+		else
+		{
+			bottle = bottle - 1;
+			bottle = bottle / exc;
+			count += bottle;
+		}
+	}
+	count++;
+	return count;
+}
+int main()
+{
+	int drink;
+	int money;
+	int exchange;
+	printf("一瓶汽水1元，两个瓶子兑换一瓶汽水，给20元可以喝多少瓶\n");
+	printf("请决定给多少钱，并规定兑换规则\n");
+	scanf("%d%d", &money, &exchange);
+	drink=drink_max(money, exchange);
+	printf("%d\n", drink);
+	return 0;
+}
