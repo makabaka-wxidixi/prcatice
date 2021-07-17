267223b01bbe56eea50bd61ff7d9ef08ab75e018
makabaka-wxidixi <537510672@qq.com>
2021-07-17 16:57:07 +0800

    memcpy函数实现.c

---
+++ memcpy函数实现.c
@@ -0,0 +1,23 @@
+//memcpyº¯ÊýÊµÏÖ
+#include<stdio.h>
+#include<assert.h>
+#include<string.h>
+void* my_memcpy(void* dest, void* src, int size)
+{
+	assert(dest && src);
+	char* ret = (char*)dest;
+	while (size--)
+	{
+		*(char*)dest = *(char*)src;
+		++(char*)dest;
+		++(char*)src;
+	}
+	return ret;
+}
+int main()
+{
+	int arr1[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int arr2[] = { 2,3,4,5,6,7 };
+	my_memcpy(arr1, arr2, sizeof(arr2));
+	return 0;
+}
