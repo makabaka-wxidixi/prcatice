0b21f0ee6a9b738ab48c3fdd04e4b4947ba36c4e
makabaka-wxidixi <537510672@qq.com>
2021-07-26 21:44:57 +0800

    移除链表元素.c

---
+++ 移除链表元素.c
@@ -0,0 +1,102 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+
+typedef struct listnode {//定义结点
+	int val;
+	struct listnode* next;
+}listnode;
+
+listnode* initlist1(int count)//头插法
+{
+	int i = 0;
+	listnode* head = (listnode*)malloc(sizeof(listnode));//头结点
+	if (head == NULL)
+	{
+		perror("initlist1 head");
+		return NULL;
+	}
+	head->next = NULL;
+	for (i = 0; i < count; i++)
+	{
+		listnode* p = (listnode*)malloc(sizeof(listnode));
+		printf("L1:请输入\n");
+		scanf("%d", &(p->val));
+		p->next = head->next;
+		head->next = p;
+	}
+	return head->next;//输出的是首元结点
+}
+
+listnode* initlist2(int count)//尾插法
+{
+	int i = 0;
+	listnode* head = (listnode*)malloc(sizeof(listnode));//建立头结点
+	if (head == NULL)
+	{
+		perror("initlist2 head");
+		return NULL;
+	}
+	head->next = NULL;
+	listnode* rear = head;//尾指针
+	for (i = 0; i < count; i++)
+	{
+		listnode* p = (listnode*)malloc(sizeof(listnode));
+		if (p == NULL)
+		{
+			perror("initlist2 p");
+			return NULL;
+		}
+		p->next = NULL;
+		printf("L2:请输入\n");
+		scanf("%d", &(p->val));
+		rear->next = p;
+		rear = p;
+	}
+	return head->next;
+}
+void printlist(listnode* l)//
+{
+	listnode* p = l;
+	while (p != NULL)
+	{
+		printf("%d ", p->val);
+		p = p->next;
+	}
+	printf("\n");
+}
+
+void removeval(listnode* l, int val)//传入的是首元结点
+{
+	assert(l);
+	listnode* head = (listnode*)malloc(sizeof(listnode));
+	if (head == NULL)
+	{
+		perror("removeval head");
+		return;
+	}
+	head->next = l;
+	while (head->next != NULL)
+	{
+		if (head->next->val == val)
+		{
+			head->next = head->next->next;
+		}
+		else
+		{
+			head = head->next;
+		}
+	}
+}
+int main()
+{
+	listnode* L1 = initlist1(7);//头插法是倒序
+	//printlist(L1);
+	listnode* L2 = initlist2(7);//尾插法是正序
+	//printlist(L2);
+	removeval(L1, 4);
+	printlist(L1);
+	removeval(L2, 3);
+	printlist(L2);
+	return 0;
+}
