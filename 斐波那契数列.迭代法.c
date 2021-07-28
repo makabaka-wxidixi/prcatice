f3e47635a80d73a75564b0f62cd35ad48fe7feee
makabaka-wxidixi <537510672@qq.com>
2021-07-28 18:29:35 +0800

    对函数进行整理优化

--- 斐波那契数列.迭代法.c
+++ 斐波那契数列.迭代法.c
@@ -1,23 +1,17 @@
-int Fibonacci2(int n)
+int Fibonacci(int n)
 {
-	int x = 0;
-	int y = 1;
-	int i = 1;
-	int ret = 0;
-	for (i = 0; i <= n; i++)
+	if (n <= 1)//如果是前两位，直接返回就行
+		return n;
+	int sum = 0;
+	int a = 0;//否则就创立连个变量来代替n=0和n=1
+	int b = 1;
+	for (int i = 1; i < n; i++)
 	{
-		if (i == 0)
-			ret = 0;
-		else if (i == 1)
-			ret = 1;
-		else
-		{
-			ret = x + y;
-			x = y;
-			y = ret;
-		}
+		sum = a + b;
+		a = b;
+		b = sum;
 	}
-	return ret;
+	return sum;
 }
 int main()
 {
@@ -25,8 +19,7 @@ int main()
 	int n = 5;
 	printf("斐波那契数列\n求第n位");
 	scanf("%d", &n);
-	//ret = Fibonacci1(n);
-	ret = Fibonacci2(n);
+	ret = Fibonacci(n);
 	printf("%d\n", ret);
 	return 0;
 }
