48b8684c4880fb73c7cadcd4953cb21263b98064
makabaka-wxidixi <537510672@qq.com>
2021-07-28 18:13:22 +0800

    将数列初始值调整为0

--- 斐波那契数列。递归.c
+++ 斐波那契数列。递归.c
@@ -1,15 +1,12 @@
 #include<stdio.h>
 int Fibonacci(int n)
 {
-	while (n)
-	{
-		if (n == 2)
-			return 1;
 		if (n == 1)
+			return 1;
+		if (n <= 0)
 			return 0;
-		if (n > 2)
+		if (n > 1)
 			return Fibonacci (n-1)+Fibonacci (n-2);
-	}
 }
 int main()
 {
