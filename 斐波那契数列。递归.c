06aefbf2ef1f0806fca6bca5ef94563611745afd
makabaka-wxidixi <537510672@qq.com>
2021-07-29 09:58:27 +0800

    代码重排版

--- 斐波那契数列。递归.c
+++ 斐波那契数列。递归.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 int Fibonacci(int n)
 {
-		if (n == 1)
-			return 1;
-		if (n <= 0)
-			return 0;
-		if (n > 1)
-			return Fibonacci (n-1)+Fibonacci (n-2);
+	if(n<=1)
+		return n;
+	if (n > 1)
+		return Fibonacci (n-1)+Fibonacci (n-2);
 }
 int main()
 {
