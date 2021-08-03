b5e67b9737ebf3a1f31eed3444fc05614d20bfb7
makabaka-wxidixi <537510672@qq.com>
2021-08-03 21:54:34 +0800

    双指针

--- 二分查找.c
+++ 二分查找.c
@@ -1,37 +1,35 @@
 #include<stdio.h>
 int Find_num(int* arr, int sz, int num)
 {
-	int m = 1;
 	int left = 0;
 	int right = sz - 1;
-	while (m)
+	while (left<=right)
 	{
-		int mid = (left + right) / 2;
-		if (arr[mid] > num)
-		{
-			right = mid - 1;
-		}
-		else if (arr[mid] < num)
-		{
+		int mid = left + (right - left) / 2;//中位数
+		if (arr[mid] < num)
 			left = mid + 1;
-		}
+		else if (arr[mid] > num)
+			right = mid - 1;
 		else
-			return mid + 1;  
+			return mid; 
 	}
+	return -1;
 }
 int main()
 {
-	int i;
-	int j;
-	int t;
-	int arr[10] = { 13,2,3,4,5,6,7,8,29,190 };
+	int i, input, sz;
+	int arr[10] = { 1,2,3,4,5,6,7,8,29,190 };
 	for (i = 0; i < 10; i++)
 	{
 		printf("%d ", arr[i]);
 	}
 	printf("输入一个数字，将会得到他的位置\n");
-	scanf("%d", &i);
-	j = sizeof(arr) / sizeof(arr[0]);
-	t = Find_num(arr,j,i);
-	printf("这个数字的位置是第%d位\n", t);
+	scanf("%d", &input);
+	sz = sizeof(arr) / sizeof(arr[0]);
+	i = Find_num(arr,sz,input);
+	if (i != -1)
+		printf("这个数字的位置是第%d位\n", i+1);
+	else
+		printf("输入数据错误\n");
 	return 0;
+}
