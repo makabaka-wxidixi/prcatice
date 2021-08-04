37174e55f21bf642478a36965c71c851f764e6d6
makabaka-wxidixi <537510672@qq.com>
2021-08-04 21:08:40 +0800

    Dynamic array，malloc

--- 动态数组画菱形.c
+++ 动态数组画菱形.c
@@ -1,32 +1,20 @@
-#define row 9
-#define lin 9
+
 #include<stdio.h>
-void itl_fun(char arr[row][lin])
+#include<stdlib.h>
+void print_fun(char** arr, int input)
 {
 	int i = 0;
 	int j = 0;
-	for (i = 0; i < row; i++)
+	for (i = 0; i < input; i++)
 	{
-		for (j = 0; j < lin; j++)
-		{
-			arr[i][j] = ' ';
-		}
-	}
-}
-void print_fun(char arr[row][lin])
-{
-	int i = 0;
-	int j = 0;
-	for (i = 0; i < row; i++)
-	{
-		for (j = 0; j < lin; j++)
+		for (j = 0; j < input; j++)
 		{
 			printf("%c", arr[i][j]);
 		}
 		printf("\n");
 	}
 }
-void upper_half(int y, char arr[row][lin])//y=n/2
+void upper_half(int y, char** arr)//y=n/2
 {
 	int i = 0;
 	int r = 1;
@@ -41,7 +29,7 @@ void upper_half(int y, char arr[row][lin])//y=n/2
 		r += 2;
 	}
 }
-void mid(int n,char arr[row][lin])
+void mid(int n,char** arr)
 {
 	int i = 0;
 	for (i = 0; i < n; i++)
@@ -49,7 +37,7 @@ void mid(int n,char arr[row][lin])
 		arr[n / 2][i] = '*';
 	}
 }
-void lower_half(int y, char arr[row][lin])//y=n/2,行数
+void lower_half(int y, char** arr)//y=n/2,行数
 {
 	int r = 1;
 	int i = 0;
@@ -64,24 +52,45 @@ void lower_half(int y, char arr[row][lin])//y=n/2,行数
 		r += 2;
 	}
 }
+char** CrearArr(int n)//动态二维数组
+{
+	char** arr = (char**)malloc(sizeof(char*) * n);
+	if (arr == NULL)	return NULL;
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = (char*)malloc(sizeof(char) * n);
+		if (arr[i] == NULL)		return NULL;
+		for (int j = 0; j < n; j++)
+		{
+			arr[i][j] = ' ';//初始化为空格
+		}
+	}
+	return arr;
+}
 void test(int n)//n为奇数
 {
-	char arr[row][lin];
-	itl_fun(arr);//初始化函数，将数组都初始化为' '
+	int i=0;
+	char** arr=CreatArr(n);
 	upper_half(n / 2,arr);//上半区
 	mid(n, arr);//中间
 	lower_half(n / 2, arr);//下半区
-	print_fun(arr);//打印函数
+	print_fun(arr,n);//打印函数
+	for (i = 0; i < n; i++)
+	{
+		free(arr[i]);
+		arr[i] = NULL;
+	}
+	free(arr);
+	arr = NULL;
 }
 int main()
 {
 	int n;
 	printf("打印一个n行的菱形\n输入n的值\n注意：n应该为奇数\n");
-	scanf("%d", &n);
 	do
 	{
-
-		if (n / 2 == 0)
+		scanf("%d", &n);
+		if (n % 2 == 0)
 		{
 			printf("应该输入奇数\n请重新输入\n");
 		}
@@ -90,7 +99,7 @@ int main()
 			test(n);
 		}
 
-	} while (n / 2 == 0);
+	} while (n);
 
 	return 0;
 }
